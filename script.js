701542cf0bbcaace36020df8284f82583fc149da
supertokensol <116369618+supertokensol@users.noreply.github.com>
2023-11-13 12:22:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -196,20 +196,4 @@ function onWindowResize() {
 }
 
 
-
-/*     Fullscreen btn     */
- let fullscreen;
- let fsEnter = document.getElementById('fullscr');
- fsEnter.addEventListener('click', function (e) {
-    e.preventDefault();
-    if (!fullscreen) {
-        fullscreen = true;
-         document.documentElement.requestFullscreen();
-         fsEnter.innerHTML = "Exit Fullscreen";
-    }
-     else {
-         fullscreen = false;
-        document.exitFullscreen();
-         fsEnter.innerHTML = "Go Fullscreen";
-    }
  });
