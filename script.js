05f39ac2e45ae2f4e3df73d69b721bb2d9ff074c
supertokensol <116369618+supertokensol@users.noreply.github.com>
2023-11-13 12:30:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -195,3 +195,9 @@ function onWindowResize() {
     renderer.setSize(container.clientWidth, container.clientHeight);
 }
 
+
+    window.addEventListener('load', fg_load)
+
+    function fg_load() {
+        document.getElementById('loading').style.display = 'none'
+    }
